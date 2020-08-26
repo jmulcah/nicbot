23c666eee065778d33185c98c355d8d716baa847
James Mulcahy <jmulcah@tcd.ie>
2020-08-26 12:13:41 +0100

    fixed weather function

--- commands/weather.js
+++ commands/weather.js
@@ -3,12 +3,19 @@ const axios = require("axios");
 module.exports = async function weather(data) {
   const request = data.text.split(" ");
   const country = request[2];
-  const res = await axios.get(
-    `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API_TOKEN}&query=${country}`
-  );
-  const temperature = res.data.current.temperature;
-  const type = res.data.current.weather_descriptions;
-  const response = `It is ${temperature} degrees with ${type} in ${country} right now.`;
-  console.log("here " + response);
-  return response;
+  return axios
+    .get(
+      `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API_TOKEN}&query=${country}`
+    )
+    .then(function (res) {
+      console.log(res);
+      const temperature = res.data.current.temperature;
+      const type = res.data.current.weather_descriptions;
+      const response = `It is ${temperature} degrees with ${type} in ${country} right now.`;
+      return response;
+    })
+    .catch(function (error) {
+      console.log(error);
+      return "Sorry I can't get the do that right now";
+    });
 };
